16871bbc05febbf5557b9aeddc8da316eb92d404
Brandon Duong <brandon.duong@outlook.com>
2023-08-26 12:28:47 -0400

    added 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -33,6 +33,14 @@ const router = createRouter({
       alias: "/",
       component: () => import("../views/LoginView.vue"),
     },
+    {
+      path: "/404",
+      component: () => import("../views/NotFoundView.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      component: () => import("../views/NotFoundView.vue"),
+    },
   ],
 });
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import router from "@/router";
+
+router.push({ path: "/404" });
+</script>
+
+<template>
+  <div class="main">404 Not Found</div>
+</template>
+
+<style scoped>
+.main {
+  grid-column: span 2;
+}
+</style>
