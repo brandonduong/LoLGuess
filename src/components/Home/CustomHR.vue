5ce0f8e36ed4a7e9329684c9d5dffb25b61b3dcd
Brandon Duong <brandon.duong@outlook.com>
2024-05-20 23:51:37 -0400

    added alt

--- src/components/Home/CustomHR.vue
+++ src/components/Home/CustomHR.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
   <div style="text-align: center">
-    <img src="/decorator-hr.png" style="max-width: 100%" />
+    <img src="/decorator-hr.png" style="max-width: 100%" alt="gold-metal-hr" />
   </div>
 </template>
