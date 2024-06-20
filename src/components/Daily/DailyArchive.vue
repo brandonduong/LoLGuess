3201af6c0dda93e6f755b8e4553ae4635cbc6fca
Brandon Duong <brandon.duong@outlook.com>
2024-06-20 00:14:21 -0400

    prepare for different sets

--- src/components/Daily/DailyArchive.vue
+++ src/components/Daily/DailyArchive.vue
@@ -12,20 +12,25 @@ defineProps<{
   option: string;
 }>();
 
-function getDailyDates() {
-  const startOfDailies = new Date("06/19/2024"); // Only support most recent set
+const nowToronto = new Date(
+  new Date().toLocaleString("en-US", {
+    timeZone: "America/Toronto",
+  })
+);
+const today = new Date(
+  nowToronto.getUTCFullYear(),
+  nowToronto.getUTCMonth(),
+  nowToronto.getUTCDate()
+);
+
+function getDailyDates(start: Date, end: Date) {
   const dailies = [];
-  const nowToronto = new Date(
-    new Date().toLocaleString("en-US", {
-      timeZone: "America/Toronto",
-    })
+  let date = new Date(
+    end.getUTCFullYear(),
+    end.getUTCMonth(),
+    end.getUTCDate()
   );
-  var date = new Date(
-    nowToronto.getUTCFullYear(),
-    nowToronto.getUTCMonth(),
-    nowToronto.getUTCDate()
-  );
-  while (date >= startOfDailies) {
+  while (date >= start && date <= end) {
     dailies.push(date.toISOString().split("T")[0]);
     date = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1);
   }
@@ -33,63 +38,70 @@ function getDailyDates() {
 }
 
 const router = useRouter();
+const sets = {
+  SET11: { start: new Date("06/10/2024"), end: today },
+  // SET10: { start: new Date("06/05/2024"), end: new Date("06/09/2024") },
+};
 </script>
 
 <template>
-  <div class="daily-grid">
-    <HomeButton
-      v-for="(d, ind) in getDailyDates()"
-      :onClick="() => router.push(`/daily/${d}/${option}`)"
-      :type="ind === 0 ? 'tertiary' : 'secondary'"
-    >
-      <div style="flex: 1; height: 100%">
-        <h5>{{ d }}</h5>
-        <div v-for="prev in [guessedBefore(d, option)]">
-          <div v-if="prev">
-            <p style="margin: 0">
-              <span
-                v-for="(place, ind) in prev.placements.map((place) =>
-                  parseInt(place)
-                )"
+  <div v-for="[key, value] in Object.entries(sets)">
+    <h5 style="margin-bottom: 0; margin-top: 1rem">{{ key }}</h5>
+    <div class="daily-grid">
+      <HomeButton
+        v-for="(d, ind) in getDailyDates(value.start, value.end)"
+        :onClick="() => router.push(`/daily/${d}/${option}`)"
+        :type="ind === 0 ? 'tertiary' : 'secondary'"
+      >
+        <div style="flex: 1; height: 100%">
+          <h5>{{ d }}</h5>
+          <div v-for="prev in [guessedBefore(d, option)]">
+            <div v-if="prev">
+              <p style="margin: 0">
+                <span
+                  v-for="(place, ind) in prev.placements.map((place) =>
+                    parseInt(place)
+                  )"
+                >
+                  <span>{{ place }}</span>
+                  <span v-if="ind != 7">, </span>
+                </span>
+              </p>
+              <div
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                "
               >
-                <span>{{ place }}</span>
-                <span v-if="ind != 7">, </span>
-              </span>
-            </p>
-            <div
-              style="
-                display: flex;
-                align-items: center;
-                justify-content: center;
-              "
-            >
-              <RankIcon :rank="prev.rank" width="2.5rem" height="2.5rem" />
-              <p style="margin: 0"><arrow-right-outlined /></p>
-              <RankIcon
-                :rank="prev.verifiedRank"
-                width="2.5rem"
-                height="2.5rem"
-              />
-            </div>
+                <RankIcon :rank="prev.rank" width="2.5rem" height="2.5rem" />
+                <p style="margin: 0"><arrow-right-outlined /></p>
+                <RankIcon
+                  :rank="prev.verifiedRank"
+                  width="2.5rem"
+                  height="2.5rem"
+                />
+              </div>
 
-            <p style="margin: 0" class="number">
-              {{
-                calculateScore(
-                  prev.placements,
-                  prev.rank,
-                  prev.verifiedRank,
-                  option === "all" ? ALL : option === "high" ? HIGH : LOW
-                )[2]
-              }}
-              %
-            </p>
-          </div>
-          <div v-else>
-            <p style="margin: 0">---</p>
+              <p style="margin: 0" class="number">
+                {{
+                  calculateScore(
+                    prev.placements,
+                    prev.rank,
+                    prev.verifiedRank,
+                    option === "all" ? ALL : option === "high" ? HIGH : LOW
+                  )[2]
+                }}
+                %
+              </p>
+            </div>
+            <div v-else>
+              <p style="margin: 0">---</p>
+            </div>
           </div>
         </div>
-      </div>
-    </HomeButton>
+      </HomeButton>
+    </div>
   </div>
 </template>
 
